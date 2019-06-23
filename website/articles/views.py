35b2e171072a6632df187a66db8d120fb19d72e3
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-23 22:15:25 +0100

    Initial commit

---
+++ website/articles/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, DetailView
+
+# Create your views here.
+
+from . models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'home.html'
+
+class ArticleDetailsView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'batman'
